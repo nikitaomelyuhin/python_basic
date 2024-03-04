2be1771ff97ff5a85b96e57da77a07318c3b431b
Nikita Omelyuhin <nikita.omelyuhin@gmail.com>
2024-03-04 21:36:29 +0300

    add tasks; add determine numbers function

--- homework_01_functions/main.py
+++ homework_01_functions/main.py
@@ -4,4 +4,31 @@ def multiply(*args, multiple = 2):
     result.append(num ** multiple)
   return result
 
-print(square(2, 10, 11, 50))
+print(multiply(2, 10, 11, 50))
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def determine_numbers(numbers, type):
+  result = []
+  for num in numbers:
+    if type == 'even':
+      if num % 2 == 0:
+        result.append(num)
+    elif type == 'odd':
+      if num % 2 != 0:
+        result.append(num)
+    elif type == 'simple':
+      if is_prime(num):
+        result.append(num)
+  return result
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(determine_numbers(numbers, 'even'))
+print(determine_numbers(numbers, 'odd'))
+print(determine_numbers(numbers, 'simple'))
